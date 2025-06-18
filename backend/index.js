3ca0afd9c2f03c45471c4e35c0adbb862c9a35e2
pankaj vishwakarma <145672950+Pankaj-vishkarma@users.noreply.github.com>
2025-06-18 17:42:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,6 @@
-const express=require('express')
 const app=require('./app')
 const PORT=process.env.PORT
-const path=require('path')
 
-const __dirnamePath = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirnamePath, 'client/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirnamePath, 'client', 'build', 'index.html'));
-  });
-}
 app.listen(PORT,()=>{
     console.log(`Server up and running on port http://localhost:${PORT}`)
 })
